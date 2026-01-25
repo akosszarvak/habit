71c57a144779b86198a949736a8c1fc9fa3a4a50
Ákos Szarvák <akos.szarvak@gmail.com>
2026-01-25 21:59:30 +0100

    feat/add auth0 authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { auth0 } from "../lib/auth0";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">Sign up</a>
+        <a href="/auth/login">Log in</a>
+      </main>
+    );
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      batman
-    </div>
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+    </main>
   );
 }
---
+++ proxy.ts
@@ -0,0 +1,13 @@
+import { auth0 } from "./lib/auth0";
+import type { NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  // Note that proxy uses the standard Request type
+  return await auth0.middleware(request);
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
