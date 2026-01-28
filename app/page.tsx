87bc76755b4380d77816fe21bc96423ef43c1b2a
Ákos Szarvák <akos.szarvak@gmail.com>
2026-01-28 20:21:54 +0100

    feat: add shadcn components, clean up structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,32 @@
 import { auth0 } from "../lib/auth0";
+import { Button } from "@/app/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await auth0.getSession();
 
   if (!session) {
     return (
-      <main>
-        <a href="/auth/login?screen_hint=signup">Sign up</a>
-        <a href="/auth/login">Log in</a>
+      <main className="">
+        <div className="flex justify-center align-center max-h gap-4">
+          <Link href="/auth/login?screen_hint=signup">
+            <Button>Sign up</Button>
+          </Link>
+
+          <Link href="/auth/login">
+            <Button variant="outline">Log in</Button>
+          </Link>
+        </div>
       </main>
     );
   }
 
   return (
     <main>
-      <h1>Welcome, {session.user.name}!</h1>
+      <h1>Welcome, {session.user.name}</h1>
+      <Link href="/auth/logout">
+        <Button variant="outline">Log out HARD</Button>
+      </Link>
     </main>
   );
 }
---
+++ app/webapp/page.tsx
@@ -0,0 +1,3 @@
+import { auth0 } from "@/lib/auth0";
+import { Button } from "@/app/components/ui/button";
+import Link from "next/link";
