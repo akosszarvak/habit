d4e75a7e3410c60ab63e6f0946937abcf819978c
Ákos Szarvák <akos.szarvak@gmail.com>
2026-01-29 07:22:05 +0100

    feat:add shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import { auth0 } from "../lib/auth0";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default async function Home() {
   const session = await auth0.getSession();
 
   if (!session) {
     return (
-      <main>
-        <a href="/auth/login?screen_hint=signup">Sign up</a>
-        <a href="/auth/login">Log in</a>
+      <main >
+        <div className="flex flex-row align-center justify-center gap-4 h-max">
+
+       <Link href="/auth/login?screen_hint=signup">
+       <Button>Sign up</Button>
+
+       </Link> 
+
+          <Link href="/auth/login">
+       <Button>Log in</Button>
+
+       </Link> 
+
+        </div>
         
       </main>
     );
